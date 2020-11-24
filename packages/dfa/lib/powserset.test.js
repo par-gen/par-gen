45420a84f313fb7d6b789403c23c503833f5e7ae
Markus Wolf <mail@markus-wolf.de>
2020-11-24 22:09:42 +0100

    feat: add error states to the dfa

--- packages/dfa/lib/powserset.test.js
+++ packages/dfa/lib/powserset.test.js
@@ -9,18 +9,28 @@ describe("fromNFA", () => {
     const description = fromNFA(nfa);
 
     expect(description).toEqual({
-      states: ["S0", "S1", "S2"],
+      states: ["S0", "S1", "S2", "S3"],
       symbols: ["a", "b"],
       transitions: {
         S0: {
           a: "S1",
+          b: "S2",
         },
         S1: {
+          a: "S2",
+          b: "S3",
+        },
+        S2: {
+          a: "S2",
+          b: "S2",
+        },
+        S3: {
+          a: "S2",
           b: "S2",
         },
       },
       start: "S0",
-      finals: ["S2"],
+      finals: ["S3"],
     });
   });
 });
