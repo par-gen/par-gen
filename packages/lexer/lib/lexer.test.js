256cd0e6f0f9c0ab5413af21c9547b57a8d7897f
Markus Wolf <mail@markus-wolf.de>
2020-12-01 22:59:56 +0100

    refactor: cleanup api

--- packages/lexer/lib/lexer.test.js
+++ packages/lexer/lib/lexer.test.js
@@ -3,35 +3,40 @@ import { lexer } from "./lexer.js";
 describe("lexer", () => {
   it("should create a lexer which is executable as evaluated function", () => {
     const code = lexer({ codegen: { module: "function" } });
+    /**
+     * @type {{next(input: Uint8Array): {success: boolean, state?: string, value?: string}}}
+     */
     const { next } = Function(code)();
 
     const input = new Uint8Array(Buffer.from("ab z"));
+    let view = input.subarray(0);
 
-    // match 1
-    const matched1 = next(input);
+    [
+      // match
+      {
+        success: true,
+        state: "A",
+        value: input.subarray(0, 2),
+      },
+      // match
+      {
+        success: true,
+        state: "WS",
+        value: input.subarray(2, 3),
+      },
+      // miss
+      {
+        success: false,
+        state: undefined,
+        value: undefined,
+      },
+    ].forEach((expected) => {
+      const matched = next(view);
+      expect(matched).toEqual(expected);
 
-    expect(matched1).toEqual({
-      success: true,
-      s: "A",
-      value: input.subarray(0, 2),
-    });
-
-    // match 1
-    const matched2 = next(input.subarray(2));
-
-    expect(matched2).toEqual({
-      success: true,
-      s: "WS",
-      value: input.subarray(2, 3),
-    });
-
-    // non-match
-    const matched3 = next(input.subarray(3));
-
-    expect(matched3).toEqual({
-      success: false,
-      s: undefined,
-      value: undefined,
+      if (matched.value) {
+        view = view.subarray(matched.value.length);
+      }
     });
   });
 });
