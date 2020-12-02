18d80a7a3f9773d5814b0465336a724eefa624fc
Markus Wolf <mail@markus-wolf.de>
2020-12-02 08:50:10 +0100

    feat: add rules to grammar

--- packages/grammar/lib/parser.test.js
+++ packages/grammar/lib/parser.test.js
@@ -6,12 +6,15 @@ describe("parse", () => {
       # terminals
 
       NUMBER := 0|1|2|3|4|5|6|7|8|9;
+      PLUS := +;
+      MINUS := -;
       
       # rules
       
-      Expression <- Plus | Minus;
-      Plus <- NUMBER '+' NUMBER;
-      Minus <- NUMBER '-' NUMBER;
+      Expression <- Plus;
+      Expression <- Minus;
+      Plus <- NUMBER PLUS NUMBER;
+      Minus <- NUMBER MINUS NUMBER;
     `);
 
     expect(result).toEqual(
@@ -20,6 +23,54 @@ describe("parse", () => {
           {
             name: "NUMBER",
             expr: "0|1|2|3|4|5|6|7|8|9",
+          },
+          {
+            name: "PLUS",
+            expr: "+",
+          },
+          {
+            name: "MINUS",
+            expr: "-",
+          },
+        ],
+      })
+    );
+  });
+
+  it("should return a list of rules fro a grammar", () => {
+    const result = parse(`
+      # terminals
+
+      NUMBER := 0|1|2|3|4|5|6|7|8|9;
+      PLUS := +;
+      MINUS := -;
+      
+      # rules
+      
+      Expression <- Plus;
+      Expression <- Minus;
+      Plus <- NUMBER PLUS NUMBER;
+      Minus <- NUMBER MINUS NUMBER;
+    `);
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        rules: [
+          {
+            name: "Expression",
+            symbols: ["Plus"],
+          },
+          {
+            name: "Expression",
+            symbols: ["Minus"],
+          },
+          {
+            name: "Plus",
+            symbols: ["NUMBER", "PLUS", "NUMBER"],
+          },
+          {
+            name: "Minus",
+            symbols: ["NUMBER", "MINUS", "NUMBER"],
           },
         ],
       })
