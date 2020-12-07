b20d41b3559ce21b6f1ec229068cd1be119a1117
Markus Wolf <mail@markus-wolf.de>
2020-12-07 22:27:24 +0100

    test: add lexer tests for esm and commonjs codegen

--- packages/lexer/lib/lexer.test.js
+++ packages/lexer/lib/lexer.test.js
@@ -1,73 +1,184 @@
+import * as vm from "vm";
+
 import { lexer } from "./lexer.js";
 
 describe("lexer", () => {
-  it("should create a lexer which is executable as evaluated function", () => {
-    const grammar = `
+  describe("with function codegen", () => {
+    it("should create a lexer which is executable as evaluated function", () => {
+      const grammar = `
       A := 'ab|ac';
       B := 'b';
       C := 'c';
       D := 'd';
     `;
-    const code = lexer(grammar, { codegen: { module: "function" } });
-    /**
-     * @type {{next(input: Uint8Array, offset: number): {success: boolean, state?: string, value?: string}}}
-     */
-    const { next } = Function(code)();
+      const code = lexer(grammar, { codegen: { module: "function" } });
+      /**
+       * @type {{next(input: Uint8Array, offset: number): {success: boolean, state?: string, value?: string}}}
+       */
+      const { next } = Function(code)();
 
-    const input = new Uint8Array(Buffer.from("abbz"));
+      const input = new Uint8Array(Buffer.from("abbz"));
 
-    [
-      // match
       [
-        {
-          state: "A",
-          start: 0,
-          end: 2,
-        },
-        0,
-      ],
-      // match
-      [
-        {
-          state: "B",
-          start: 2,
-          end: 3,
-        },
-        2,
-      ],
-      // miss
-      [
-        {
-          state: undefined,
-          start: -1,
-          end: -1,
-        },
-        3,
-      ],
-    ].forEach(([expected, offset]) => {
-      const matched = next(input, /** @type {number} */ (offset));
-      expect(matched).toEqual(expected);
+        // match
+        [
+          {
+            state: "A",
+            start: 0,
+            end: 2,
+          },
+          0,
+        ],
+        // match
+        [
+          {
+            state: "B",
+            start: 2,
+            end: 3,
+          },
+          2,
+        ],
+        // miss
+        [
+          {
+            state: undefined,
+            start: -1,
+            end: -1,
+          },
+          3,
+        ],
+      ].forEach(([expected, offset]) => {
+        const matched = next(input, /** @type {number} */ (offset));
+        expect(matched).toEqual(expected);
+      });
+    });
+
+    it("should create a lexer with support for escaped tokens", () => {
+      const grammar = `
+      A := '\\|';
+    `;
+      const code = lexer(grammar, { codegen: { module: "function" } });
+      /**
+       * @type {{next(input: Uint8Array, offset: number): {success: boolean, state?: string, value?: string}}}
+       */
+      const { next } = Function(code)();
+
+      const input = new Uint8Array(Buffer.from("|"));
+
+      const matched = next(input, /** @type {number} */ (0));
+
+      expect(matched).toEqual({
+        state: "A",
+        start: 0,
+        end: 1,
+      });
     });
   });
 
-  it("should create a lexer with support for escaped tokens", () => {
-    const grammar = `
-      A := '\\|';
-    `;
-    const code = lexer(grammar, { codegen: { module: "function" } });
-    /**
-     * @type {{next(input: Uint8Array, offset: number): {success: boolean, state?: string, value?: string}}}
-     */
-    const { next } = Function(code)();
+  describe("with ESM codegen", () => {
+    it("should be possible to write a lexer into a file", async () => {
+      let results;
+      /** @type {(_results: *) => void}  */
+      const output = (_results) => {
+        results = _results;
+      };
 
-    const input = new Uint8Array(Buffer.from("|"));
+      const grammar = `
+        A := 'abc';
+      `;
+      const code = lexer(grammar, { codegen: { module: "esm" } });
 
-    const matched = next(input, /** @type {number} */ (0));
+      // todo: this is not available in the typescript typings currently
+      const SourceTextModule = /** @type {*} */ (vm).SourceTextModule;
+      const context = vm.createContext({
+        output,
+        Buffer,
+      });
 
-    expect(matched).toEqual({
-      state: "A",
-      start: 0,
-      end: 1,
+      const module = new SourceTextModule(
+        `
+          import { next } from 'lexer-module';
+
+          const input = new Uint8Array(Buffer.from("abc"));
+          const matched = next(input, 0);
+
+          output(matched);
+        `,
+        { context }
+      );
+
+      await module.link(
+        /**
+         * @param {string} specifier
+         * @param {*} referencingModule
+         */
+        function (specifier, referencingModule) {
+          if (specifier === "lexer-module") {
+            return new SourceTextModule(code, {
+              context: referencingModule.context,
+            });
+          }
+          throw new Error(`Unable to resolve dependency: ${specifier}`);
+        }
+      );
+
+      await module.evaluate();
+
+      expect(results).toEqual({
+        state: "A",
+        start: 0,
+        end: 3,
+      });
+    });
+  });
+
+  describe("with commonjs codegen", () => {
+    it("should be possible to write a lexer into a file", async () => {
+      let results;
+      /** @type {(_results: *) => void}  */
+      const output = (_results) => {
+        results = _results;
+      };
+
+      const grammar = `
+        A := 'abc';
+      `;
+      const code = lexer(grammar, { codegen: { module: "commonjs" } });
+
+      const context = vm.createContext({
+        output,
+        Buffer,
+        /**
+         * @param {string} id
+         */
+        require(id) {
+          if (id === "lexer-module") {
+            const context = vm.createContext({ module: {} });
+            new vm.Script(code).runInContext(context);
+            return context.module.exports;
+          }
+          throw new Error(`Unable to resolve dependency: ${id}`);
+        },
+      });
+
+      const script = new vm.Script(
+        `
+          const { next } = require('lexer-module');
+
+          const input = new Uint8Array(Buffer.from("abc"));
+          const matched = next(input, 0);
+
+          output(matched);
+        `
+      );
+
+      script.runInContext(context);
+
+      expect(results).toEqual({
+        state: "A",
+        start: 0,
+        end: 3,
+      });
     });
   });
 });
