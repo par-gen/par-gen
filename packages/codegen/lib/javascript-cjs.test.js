a0121dacec4c3d7c8c1e48b7410339eae2fbfc78
Markus Wolf <mail@markus-wolf.de>
2020-12-09 18:58:13 +0100

    feat: create commonjs code generator

--- packages/codegen/lib/javascript-cjs.test.js
+++ packages/codegen/lib/javascript-cjs.test.js
@@ -1,4 +1,5 @@
-import { generate } from "@knisterpeter/expound-lexer";
+import { generate as genLexer } from "@knisterpeter/expound-lexer";
+import { generate as genParser } from "@knisterpeter/expound-parser";
 import { promises as fsp, readFileSync } from "fs";
 import { tmpdir } from "os";
 import { join } from "path";
@@ -9,9 +10,15 @@ import { JavaScriptCommonJsCodegen } from "./javascript-cjs.js";
 describe("JavaScriptCommonJsCodegen", () => {
   /** @type {string} */
   let directory;
+  /** @type {string} */
+  let lexerFile;
+  /** @type {string} */
+  let parserFile;
 
   beforeEach(async () => {
     directory = await fsp.mkdtemp(join(tmpdir(), "lexer-"));
+    lexerFile = join(directory, "lexer.js");
+    parserFile = join(directory, "parser.js");
   });
 
   afterEach(async () => {
@@ -19,8 +26,6 @@ describe("JavaScriptCommonJsCodegen", () => {
   });
 
   it("should be possible to write a lexer into a file", async () => {
-    const lexerFile = join(directory, "lexer.js");
-
     let results;
     /** @type {(_results: *) => void}  */
     const output = (_results) => {
@@ -31,9 +36,9 @@ describe("JavaScriptCommonJsCodegen", () => {
       A := 'abc';
     `;
 
-    const codegen = new JavaScriptCommonJsCodegen({ lexerFile });
+    const codegen = new JavaScriptCommonJsCodegen({ lexerFile, parserFile });
 
-    await codegen.lexer(generate(grammar));
+    await codegen.lexer(genLexer(grammar));
 
     const context = vm.createContext({
       output,
@@ -68,6 +73,76 @@ describe("JavaScriptCommonJsCodegen", () => {
       state: "A",
       start: 0,
       end: 3,
+    });
+  });
+
+  it("should be possible to write a parser into a file", async () => {
+    let results;
+    /** @type {(_results: *) => void}  */
+    const output = (_results) => {
+      results = _results;
+    };
+
+    const grammar = `
+      A := 'abc';
+
+      Rule := A;
+    `;
+
+    const codegen = new JavaScriptCommonJsCodegen({ lexerFile, parserFile });
+    await codegen.parser(genParser(grammar));
+
+    const context = vm.createContext({
+      output,
+      Buffer,
+      /**
+       * @param {string} id
+       */
+      require(id) {
+        if (id === "./lexer.js") {
+          const innerContext = vm.createContext({
+            module: {},
+          });
+          new vm.Script(readFileSync(lexerFile, "utf-8")).runInContext(
+            innerContext
+          );
+          return innerContext.module.exports;
+        } else if (id === parserFile) {
+          const innerContext = vm.createContext({
+            module: {},
+            require: context.require,
+            Buffer: context.Buffer,
+          });
+          new vm.Script(readFileSync(parserFile, "utf-8")).runInContext(
+            innerContext
+          );
+          return innerContext.module.exports;
+        }
+        throw new Error(`Unable to resolve dependency: ${id}`);
+      },
+    });
+
+    const script = new vm.Script(
+      `
+        const { parse } = require('${parserFile}');
+
+        const matched = parse("abc");
+
+        output(matched);
+      `
+    );
+
+    script.runInContext(context);
+
+    expect(results).toEqual({
+      name: "Rule",
+      items: [
+        {
+          name: "A",
+          start: 0,
+          end: 3,
+        },
+      ],
     });
   });
 });
