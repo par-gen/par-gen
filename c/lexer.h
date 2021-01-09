bf96f376d05f930b863a8c10dcd0eeb013268da6
ZauberNerd <zaubernerd@zaubernerd.de>
2021-01-09 18:48:08 +0100

    wip: implement json parser in c

---
+++ c/lexer.h
@@ -0,0 +1,14 @@
+#define undefined (-1)
+
+struct lexeme
+{
+  unsigned int state;
+  int start;
+  int end;
+};
+
+extern struct lexeme lexeme;
+
+void lexer_next(const char *input, const unsigned int offset,
+                const unsigned int l);
+void init_lexer();
