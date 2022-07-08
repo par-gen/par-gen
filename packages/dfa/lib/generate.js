98fe86b6bfa2dd1b3be098eb16c2ea55a165db7c
Björn Brauer <bjoern.brauer@new-work.se>
2022-07-08 21:08:52 +0000

    test: new and bigger regex to test stuff

--- packages/dfa/lib/generate.js
+++ packages/dfa/lib/generate.js
@@ -5,21 +5,24 @@ import prettier from "prettier";
 import { DFA } from "./dfa.js";
 
 const dfa = DFA.fromNFA(
-  NFA.fromRegExp("ab(c|d)(e|f)*((gh|ij)(kl|mn))*")
+  NFA.fromRegExp(
+    `(-|)(0|[1-9][0-9]*)(\\.[0-9][0-9]*|)([Ee]([-+]|)(0|[1-9][0-9]*)|)`
+  )
 ).minimal();
 
 console.log(dfa.toString());
 
 let code = dfa.compileToString((symbol) => symbol.charCodeAt(0));
 
+// TODO: wtf is wrong with the leading zero?
 code += `
-const input = Buffer.from("abcffghmnijmn");
+const input = Buffer.from("-1234.7281397237123872189037218937e+3213213213213123");
 
 console.log(test(input));
-console.log(test(Buffer.from("abdffghmnijmn")));
-console.log(test(Buffer.from("abc")));
-console.log(test(Buffer.from("xyz")));
-console.log(test(Buffer.from("abcdefgh")));
+console.log(test(Buffer.from("1")));
+console.log(test(Buffer.from("023243254890.3213213213e+132313")));
+console.log(test(Buffer.from("1.e")));
+console.log(test(Buffer.from("1.e+5")));
 
 (function warmup() {
   for (let i = 0; i < 1_000; i++) {
